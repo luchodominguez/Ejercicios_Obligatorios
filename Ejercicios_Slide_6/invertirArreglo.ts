bbc15ca7ed187cf5f05012fccc054df5ac1c97d0
Luciano Dominguez <lunora132@gmail.com>
2024-09-28 17:03:01 -0300

    Ejercicio 4 - invertirArreglo

---
+++ Ejercicios_Slide_6/invertirArreglo.ts
@@ -0,0 +1,27 @@
+/*
+• Almacene en un arreglo de tamaño N los números ingresados
+por el usuario
+• La dimensión N también es ingresada por el usuario
+• Muestre los números del arreglo pero del último al primero
+*/
+
+import * as fs from "readline-sync";
+import { dibujarLinea } from "../dibujarLinea";
+console.clear();
+
+const DIM: number = fs.questionInt("Ingrese la dimension del arreglo: ");
+dibujarLinea(60);
+const NUMEROS: number[] = new Array(DIM);
+
+for (let i: number = 0; i < NUMEROS.length; i++) {
+  NUMEROS[i] = fs.questionInt(`Ingrese un numero (#${i + 1} de ${DIM}): `);
+}
+
+dibujarLinea(60, true);
+console.log("Array direccion correcta:");
+console.log(NUMEROS);
+dibujarLinea(60);
+
+console.log("Array direccion invertida:");
+console.log(NUMEROS.reverse());
+dibujarLinea(60, true);
